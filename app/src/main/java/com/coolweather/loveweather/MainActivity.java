0e5d6673a81752157d65e8ef80b800bb1b48f101
LFY <1064056026@qq.com>
2020-11-13 10:18:32 +0800

    第三次

---
+++ app/src/main/java/com/coolweather/loveweather/MainActivity.java
@@ -0,0 +1,18 @@
+package com.coolweather.loveweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.litepal.LitePal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LitePal.initialize(this);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
